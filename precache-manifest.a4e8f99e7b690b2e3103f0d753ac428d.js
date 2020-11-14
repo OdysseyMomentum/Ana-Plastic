39899eb0b0d7a53262aef4d31a8cc12bc5d938a9
Djamaile Rahamat <drahamat@bol.com>
2020-11-14 11:31:16 +0100

    Updates

---
+++ precache-manifest.a4e8f99e7b690b2e3103f0d753ac428d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a0410fa871bc98e5980065df6b997c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a0975fcebc2acd7c9a",
+    "url": "/static/css/4.958e3fcb.chunk.css"
+  },
+  {
+    "revision": "0429f1a644b02d06b9fc",
+    "url": "/static/css/main.d9e75851.chunk.css"
+  },
+  {
+    "revision": "ea78ca46582c0157135b",
+    "url": "/static/js/2.9e5a5f0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e5a5f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce79817523abf90b0bd6",
+    "url": "/static/js/3.09cad2bf.chunk.js"
+  },
+  {
+    "revision": "4abc431f9187b99e6ea057d85465ce6e",
+    "url": "/static/js/3.09cad2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a0975fcebc2acd7c9a",
+    "url": "/static/js/4.78989667.chunk.js"
+  },
+  {
+    "revision": "c0f5a2ebc6197b44da80",
+    "url": "/static/js/5.172ad841.chunk.js"
+  },
+  {
+    "revision": "0429f1a644b02d06b9fc",
+    "url": "/static/js/main.00a4f604.chunk.js"
+  },
+  {
+    "revision": "2188ac2fcc28adfe3d48",
+    "url": "/static/js/runtime-main.2aba0f56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
