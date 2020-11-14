99d2a92f5ab8a32efc5c57cc65c1683c13b1292c
djamaile <rdjamaile@gmail.com>
2020-11-14 14:21:47 +0100

    Diamonds (#23)
    * feat: diamonds test
    
    * feat: diamonds test
    
    Co-authored-by: Djamaile Rahamat <drahamat@bol.com>

--- src/components/game/Scene.ts
+++ src/components/game/Scene.ts
@@ -1,19 +1,38 @@
 import "phaser";
-import map from "../../assets/images/map.png";
+import diamond from "../../assets/images/diamond.png";
+import map from "../../assets/images/largemap.png";
 
 class Scene extends Phaser.Scene {
   constructor() {
     super("scene");
   }
 
-  preload() {
+  loadDiamonds(amountOfDiamonds: number) {
+    for (let i = 0; i < amountOfDiamonds; i = i + 1) {
+      const x: number = Phaser.Math.Between(0, 2048);
+      const y: number = Phaser.Math.Between(0, 2048);
 
-    this.load.image('map', map);
+      const box = this.add.image(x, y, diamond);
+
+      box.setInteractive();
+      box.on("clicked", this.clickHandler, this);
+    }
+  }
+
+  clickHandler(box?: any) {
+    box.off("clicked", this.clickHandler);
+    box.input.enabled = false;
+    box.setVisible(false);
+  }
+
+  preload() {
+    this.load.image("map", map);
   }
 
   create() {
     this.cameras.main.setBounds(0, 0, 2048, 2048);
-    this.add.image(0, 0, 'map').setOrigin(0);
+    this.add.image(0, 0, "map").setOrigin(0);
+    this.loadDiamonds(20);
   }
 }
 
